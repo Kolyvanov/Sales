21fd87b52fb0aac96f7b7a460ffa7e942c54869c
Kolyvanov <Kolyvanovsl80@mail.ru>
2022-07-22 15:21:45 +0300

    int to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] a = {1, 3, 5, 7, 21};
+        long[] a = {1, 3, 5, 7, 21};
         SalesManager manager = new SalesManager(a);
         System.out.println(manager.max());
     }
